634b51d27b0827febf665cb520b47b7fefeaa3fe
penayoalbi <albi.penayo@gmail.com>
2023-05-30 16:03:29 -0300

    casi finalizado

--- VECTORES/EstacionamientoVehiculo.cpp
+++ VECTORES/EstacionamientoVehiculo.cpp
@@ -8,109 +8,164 @@ using namespace std;
 
 #define TOPE 10
 
-struct vecEst{
+struct vehiculo{
     string patente;
-    string hora;
-}est[10];
+    int hora;
+};
 
-void Menu();
-void ingreso();
-void salida();
-void listar();
-void finalizar();
+void Inicializa(vehiculo est[]);
+void Menu(vehiculo est[],int posLibre);
+void ingreso(vehiculo est[],int posLibre);
+int buscarLibre(vehiculo est[],int &posLibre);
+bool validarHora(int vhora);
+void salida(vehiculo est[],int posLibre);
+void listar(vehiculo est[],int posLibre);
+void finalizar(vehiculo est[],int posLibre);
 
-void Menu(){
+void Inicializa(vehiculo est[]){
+    for(int i=0; i<3;i++){
+        est[i].patente="libre";
+    }
+    
+}
+
+void Menu(vehiculo est[],int posLibre){
   //  system("cls");
     int opcion=0;
-    cout<<"Elija una opcion"<<endl;
-    cout<<"Opcion 1: ingreso de Vehiculo."<<endl;
-    cout<<"Opcion 2: Salida de Vehiculo."<<endl;
-    cout<<"Opcion 3: Listado de Vehiculo."<<endl;
-    cout<<"Opcion 4: Finalizar."<<endl;
+    cout<<" \n\n";
+    cout<<"\t ***Elija una opcion***"<<endl;
+    cout<<"\t Opcion 1: ingreso de Vehiculo."<<endl;
+    cout<<"\t Opcion 2: Salida de Vehiculo."<<endl;
+    cout<<"\t Opcion 3: Listado de Vehiculo."<<endl;
+    cout<<"\t Opcion 4: Finalizar."<<endl;
     
    do{
-        cout<<"Ingrese una opcion: ";
+        cout<<"\t Ingrese una opcion:  ";
         cin>> opcion;
     }while(opcion!=1 && opcion!=2 && opcion!=3 &&opcion!=4);
 
     switch ( opcion){
-        case 1: ingreso();
+        case 1: ingreso(est,posLibre);
             break;
-        case 2: salida();
+        case 2: salida(est,posLibre);
             break;
-        case 3: listar();
+        case 3: listar(est,posLibre);
             break;
-        case 4: finalizar();
+        case 4: finalizar(est,posLibre);
             break;    
     }
 }
 
-void ingreso(){
+void ingreso(vehiculo est[], int posLibre){
     
     int i=0;
-    
-    while (i<3 ){
+    //int posLibre = 0; 
+   // char corte;
+    posLibre= buscarLibre(est,posLibre);
+    cout<<" pos libre: "<<posLibre;
+
+    while (i<3 && posLibre!=-1 )
+    { 
+        cout<<posLibre<<endl;
         cout<<"Ingrese patente: ";
         cin>>est[i].patente;
-
-        cout<<"Ingrese hora de entrada: ";
-        cin>>est[i].hora;
+        do{
+            cout<<"Ingrese hora de entrada: ";
+            cin>>est[i].hora;
+        }while(validarHora(est[i].hora)!=true);
+        //cout<<" Desea ingresar otro vehiculo: S o N ";
+       // cin>>corte;
         i++;
     }
-    Menu();
+    Menu(est,posLibre);
 }
 
-void salida(){
-    
-    string patte;
+void salida(vehiculo est[],int posLibre){
+    int aux=0;
+    string aSalir;
     cout<<"Ingrese patente: ";
-    cin>>patte;
+    cin>>aSalir;
 
     for(int i=0; i<3;i++){
-        if(est[i].patente==patte){
-            cout<<" el patente: "<<patte<<"ingreso a las :"<<est[i].hora<<"hora"<<endl;
-            est[i].patente="";
+        if(est[i].patente==aSalir){
+            cout<<" el patente: "<<aSalir<<" ingreso a las "<<est[i].hora<<" hora "<<endl;
+            est[i].patente="libre";
+            est[i].hora=0;
+        }else{
+            aux=-1;
         }
     }
-    Menu();
+    if(aux==-1){
+        cout<<"El patente: "<<aSalir<<" no se encuentra!."<<endl;
+    }else{
+        cout<<"El patente ya salio!. "<<endl;
+    }
+    Menu(est,posLibre);
 
 }
 
-void listar(){
+void listar(vehiculo est[],int posLibre){
     system("cls");
     for(int i=0;i<3;i++){
-        cout<<" \n"<<est[i].patente<<" hora entrada:"<<est[i].hora;
+        if(est[i].patente!="libre"&& est[i].hora!=0){
+            cout<<" \n"<<est[i].patente<<" hora entrada: "<<est[i].hora<<endl;
+        }else{
+            cout<<"El estacionamineto esta libre!.\n";
+        }
     }
 
-    Menu();
+    Menu(est,posLibre);
 }
 
-void finalizar(){
+void finalizar(vehiculo est[], int posLibre){
+    int fin=0;
     system("cls");
     for(int i=0; i<3;i++){
-        if(est[i].patente==""){
-            cout<<"El estacionamineto esta vacio!!";
+        if(est[i].patente=="libre"){
+            //cout<<"El estacionamineto esta vacio!!"<<endl;
+            fin=0;
+        }else{
+            fin=1;
+        }
+        if(fin!=0){
+            cout<<"NO SE PUEDE FINALIZAR!!. El estacionamiento tiene que estar vacio!"<<endl;
+            Menu(est,posLibre);
         }
     }
-    cout<<"Gracias por elegirnos!";
+    cout<<"Gracias por elegirnos!\n";
 }
 
-int buscarLibre(string estVec[]){
-    int poslib=0;
+int buscarLibre(vehiculo est[], int &posLibre){
+    //int poslib=0;
 
     for(int i=0; i<3; i++){
-        if(estVec[i]=="libre"){
-            poslib=i;
+        if(est[i].patente=="libre"){
+            posLibre=i;
+            return posLibre;
         }
-    }
-    return poslib;
+       else{
+            return -1;
+        }  
+    } 
+}
+bool validarHora(int vhora){
+    if(vhora<7 || vhora>21){
+            return false;
+    }else{
+            return true;
+        }
+
 }
 
 int main(){
 
-    Menu();
-   // buscarLibre(estVec);
-  // getchar();
-   return 0;
+    vehiculo est[TOPE];
+    Inicializa(est);
+    int posLibre=0;
+
+    Menu(est,posLibre);
+    buscarLibre(est,posLibre);
+    system("PAUSE");
+    return 0;
     
 }
